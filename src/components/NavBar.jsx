1a47efdf65cbebcfb5bbc7447f0882f308c8f0ee
vmhowley <vmhowleyh@gmail.com>
2024-01-18 03:00:46 -0400

    responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 import vitelogo from '../assets/react.svg'
 function Navbar() {
   return (
-    <header className='p-4'>
-      <nav className='flex '>
-        <div className='flex  flex-col relative left-72'>
+    <header className='p-6'>
+      <nav className='flex justify-between'>
+        <div className=' transition-all opacity-0  sm:opacity-100 flex-col xl:relative xl:translate-x-72  '>
           <div className='flex gap-1'>
             <p className='text-neutral-500'>Pages / </p>
           <h1> Dashboard</h1>
           </div>
           <h1 className='font-bold'>Dashboard</h1>
         </div>
+        <div className=' sm:flex  xl:-translate-y-0 overflow-y-auto antialiased transition-opacity duration-500 -translate-x-full   xl:opacity-0 cursor-pointer '>&#9776;</div>
       </nav>
     </header>
   )
--- src/components/SideBarItems.jsx
+++ src/components/SideBarItems.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function SideBarItems({text, icon, active, path}) {
   return (
-    <li className={`relative flex items-center py-2 px-3 my-1 h-14  rounded-xl transition-colors ${active ? "bg-blue-500 bg-opacity-10 font-semibold":null}`}>
+    <li className={`relative flex items-center py-2 px-3 my-1 h-14  rounded-md transition-colors ${active ? "bg-blue-400 bg-opacity-10 font-semibold":null}`}>
               <Link className='w-full' to={path}>{text}</Link>
             </li>
   )

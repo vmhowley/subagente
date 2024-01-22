a8a59161a893d1d265ef986a5bdf8ca2181fcf04
Victor Manuel Morillo Howley <victormh@IBSYSTEMS.COM>
2024-01-22 11:40:49 -0400

    last

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,62 +2,50 @@ import React from "react";
 import LocalAtmIcon from "@mui/icons-material/LocalAtm";
 function Hero() {
   return (
-    <div className=" pl-6 relative gap-3 grid sm:grid-cols-2 xl:flex justify-center">
-      <div className=" p-4 bg-white h-[125px] w-60 sm:w-72 rounded-xl flex flex-col justify-center relative">
-        <div className="">
-        <div className="w-max">
-          <h1 className="text-neutral-500 font-semibold">TODAY'S MONEY</h1>
-          <h1 className="font-bold text-xl">$53,000</h1>
-          <div className="flex flex-wrap gap-1">
-            <p className="text-green-500 font-bold">+55%</p>
-            <p className="text-neutral-500">since yesterday</p>
-          </div>
+    <div className=" pl-6 relative gap-3 grid sm:grid-cols-2 xl:flex justify-around">
+      <div className=" p-4 bg-white h-[125px] w-60 sm:w-96 rounded-xl flex flex-col justify-center relative">
+        <h1 className="text-neutral-500 font-semibold text-sm">TODAY'S MONEY</h1>
+        <h1 className="font-bold text-xl">$53,000</h1>
+        <div className="flex flex- gap-1">
+          <p className="text-green-500 font-bold">+55%</p>
+          <p className="text-neutral-500 ">since yesterday</p>
         </div>
-        <div className="px-3 ">
-        </div>
-        </div>
-      </div>  
-      <div className=" p-4 bg-white h-[125px] w-60 sm:w-72 rounded-xl flex flex-col justify-center relative">
-        <div className="">
-        <div className="w-max">
-          <h1 className="text-neutral-500 font-semibold">TODAY'S USERS</h1>
-          <h1 className="font-bold text-xl">$2,300</h1>
-          <div className="flex flex-wrap gap-1">
-            <p className="text-green-500 font-bold">+55%</p>
-            <p className="text-neutral-500">since yesterday</p>
-          </div>
-        </div>
-        <div className="px-3 ">
-        </div>
+        <div className="flex absolute right-4 bottom-14  rounded-full bg-gradient-to-tl from-[#5e72e4] to-[#825ee4]     items-center w-14 h-14 justify-center">
+          <LocalAtmIcon sx={{ color: "white" }} className="" />
         </div>
       </div>
-      <div className=" p-4 bg-white h-[125px] w-60 sm:w-72 rounded-xl flex flex-col justify-center relative">
-        <div className="">
-        <div className="w-max">
-          <h1 className="text-neutral-500 font-semibold">TODAY'S MONEY</h1>
-          <h1 className="font-bold text-xl">$53,000</h1>
-          <div className="flex flex-wrap gap-1">
-            <p className="text-green-500 font-bold">+55%</p>
-            <p className="text-neutral-500">since yesterday</p>
-          </div>
+      <div className=" p-4 bg-white h-[125px] w-60 sm:w-96 rounded-xl flex flex-col justify-center relative">
+        <h1 className="text-neutral-500 font-semibold">TODAY'S USERS</h1>
+        <h1 className="font-bold text-xl">3,100</h1>
+        <div className="flex flex- gap-1">
+          <p className="text-green-500 font-bold">+5%</p>
+          <p className="text-neutral-500 ">since last week</p>
         </div>
-        <div className="px-3 ">
-        </div>
+        <div className="flex absolute right-4 bottom-14  rounded-full bg-gradient-to-tl from-[#5e72e4] to-[#825ee4]     items-center w-14 h-14 justify-center">
+          <LocalAtmIcon sx={{ color: "white" }} className="" />
         </div>
       </div>
-      <div className=" p-4 bg-white h-[125px] w-60 sm:w-72 rounded-xl flex flex-col justify-center relative">
-        <div className="">
-        <div className="w-max">
-          <h1 className="text-neutral-500 font-semibold">TODAY'S MONEY</h1>
-          <h1 className="font-bold text-xl">$53,000</h1>
-          <div className="flex flex-wrap gap-1">
-            <p className="text-green-500 font-bold">+55%</p>
-            <p className="text-neutral-500">since yesterday</p>
-          </div>
+      <div className=" p-4 bg-white h-[125px] w-60 sm:w-96 rounded-xl flex flex-col justify-center relative">
+        <h1 className="text-neutral-500 font-semibold">NEW CLIENTS</h1>
+        <h1 className="font-bold text-xl">+100</h1>
+        <div className="flex flex- gap-1">
+          <p className="text-green-500 font-bold">-2%</p>
+          <p className="text-neutral-500 ">since last quarter</p>
         </div>
-        <div className="px-3 ">
+        <div className="flex absolute right-4 bottom-14  rounded-full bg-gradient-to-tl from-[#5e72e4] to-[#825ee4]     items-center w-14 h-14 justify-center">
+          <LocalAtmIcon sx={{ color: "white" }} className="" />
         </div>
+      </div>
+      <div className=" p-4 bg-white h-[125px] w-60 sm:w-96 rounded-xl flex flex-col justify-center relative">
+        <h1 className="text-neutral-500 font-semibold">TODAY'S USERS</h1>
+        <h1 className="font-bold text-xl">3,200</h1>
+        <div className="flex flex- gap-1">
+          <p className="text-green-500 font-bold">+5%</p>
+          <p className="text-neutral-500 ">since last week</p>
         </div>
+      </div>
+      <div className="flex absolute right-4 bottom-14  rounded-full bg-gradient-to-tl from-[#5e72e4] to-[#825ee4]     items-center w-14 h-14 justify-center">
+        <LocalAtmIcon sx={{ color: "white" }} className="" />
       </div>
     </div>
   );
--- src/components/SideBarItems.jsx
+++ src/components/SideBarItems.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 function SideBarItems({ text, icon, active, path }) {
   return (
     <li
-      className={`relative flex items-center py-2 px-3 my-1 h-14  rounded-md transition-colors gap-3 ${
+      className={`relative flex items-center py-2 px-3 my-1 h-14  rounded-md transition-all ease-in-out  duration-500 ${
         active ? "bg-blue-400 bg-opacity-10 font-semibold" : null
       }`}
     >
+      <Link className="w-full flex gap-2" to={path}>
       {icon}
-      <Link className="w-full" to={path}>
         {text}
       </Link>
     </li>

f4ba9c676395dd6ca00cf70e6293ca86fc6acf8b
Victor Manuel Morillo Howley <victormh@IBSYSTEMS.COM>
2024-01-23 12:07:16 -0400

    Fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,8 +15,8 @@ function Navbar() {
   }
   return (
     <header className="flex p-6   ">
-      <nav className="flex justify-between">
-        <div className="transition-all w-full sm:translate-x-0 flex-col  xl:translate-x-72 z-50 text-white font-extralight">
+      <nav className="flex justify-between w-full">
+        <div className="transition-all w-max sm:translate-x-0 flex-col  xl:translate-x-72 z-50 text-white font-extralight">
           <div className="flex gap-1 ">
             <p className="text-zinc-200">Pages  </p>
             <h1>  /{Location}</h1>

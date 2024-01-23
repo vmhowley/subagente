40d004dc2bbcf4bd9294dfdfff4d519398b51e22
Victor Manuel Morillo Howley <victormh@IBSYSTEMS.COM>
2024-01-23 11:51:26 -0400

    Test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import LocalAtmIcon from "@mui/icons-material/LocalAtm";
 function Hero() {
   return (
-    <div className=" relative gap-3 grid sm:grid-cols-2 xl:flex justify-around">
-      <div className=" p-4 bg-white h-[125px] w-60 sm:w-96 rounded-xl flex flex-col justify-center relative">
-        <h1 className="text-neutral-500 font-semibold text-sm">TODAY'S MONEY</h1>
+    <div className=" relative gap-3 grid sm:grid-cols-2 sm:place-items-center xl:flex justify-center">
+      <div className=" p-4 bg-white h-[125px] w-60 sm:w-80 md:w-96 rounded-xl flex flex-col justify-center relative">
+        <h1 className="text-neutral-500 font-semibold text-sm">
+          TODAY'S MONEY
+        </h1>
         <h1 className="font-bold text-xl">$53,000</h1>
         <div className="flex flex- gap-1">
           <p className="text-green-500 font-bold">+55%</p>
@@ -14,7 +16,7 @@ function Hero() {
           <LocalAtmIcon sx={{ color: "white" }} className="" />
         </div>
       </div>
-      <div className=" p-4 bg-white h-[125px] w-60 sm:w-96 rounded-xl flex flex-col justify-center relative">
+      <div className=" p-4 bg-white h-[125px] w-60 sm:w-80 md:w-96 rounded-xl flex flex-col justify-center relative">
         <h1 className="text-neutral-500 font-semibold">TODAY'S USERS</h1>
         <h1 className="font-bold text-xl">3,100</h1>
         <div className="flex flex- gap-1">
@@ -25,7 +27,7 @@ function Hero() {
           <LocalAtmIcon sx={{ color: "white" }} className="" />
         </div>
       </div>
-      <div className=" p-4 bg-white h-[125px] w-60 sm:w-96 rounded-xl flex flex-col justify-center relative">
+      <div className=" p-4 bg-white h-[125px] w-60 sm:w-80 md:w-96 rounded-xl flex flex-col justify-center relative">
         <h1 className="text-neutral-500 font-semibold">NEW CLIENTS</h1>
         <h1 className="font-bold text-xl">+100</h1>
         <div className="flex flex- gap-1">
@@ -36,16 +38,16 @@ function Hero() {
           <LocalAtmIcon sx={{ color: "white" }} className="" />
         </div>
       </div>
-      <div className=" p-4 bg-white h-[125px] w-60 sm:w-96 rounded-xl flex flex-col justify-center relative">
-        <h1 className="text-neutral-500 font-semibold">TODAY'S USERS</h1>
-        <h1 className="font-bold text-xl">3,200</h1>
+      <div className=" p-4 bg-white h-[125px] w-60 sm:w-80 md:w-96 rounded-xl flex flex-col justify-center relative">
+        <h1 className="text-neutral-500 font-semibold">NEW CLIENTS</h1>
+        <h1 className="font-bold text-xl">+100</h1>
         <div className="flex flex- gap-1">
-          <p className="text-green-500 font-bold">+5%</p>
-          <p className="text-neutral-500 ">since last week</p>
+          <p className="text-green-500 font-bold">-2%</p>
+          <p className="text-neutral-500 ">since last quarter</p>
         </div>
-      </div>
-      <div className="flex absolute right-4 bottom-14  rounded-full bg-gradient-to-tl from-[#5e72e4] to-[#825ee4]     items-center w-14 h-14 justify-center">
-        <LocalAtmIcon sx={{ color: "white" }} className="" />
+        <div className="flex absolute right-4 bottom-14  rounded-full bg-gradient-to-tl from-[#5e72e4] to-[#825ee4]     items-center w-14 h-14 justify-center">
+          <LocalAtmIcon sx={{ color: "white" }} className="" />
+        </div>
       </div>
     </div>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,9 +4,10 @@ import Logo from "../assets/images/logo.png"
 import { useLocation } from "react-router-dom"
 import SideBarItems from "./SideBarItems"
 import PersonalVideoIcon from '@mui/icons-material/PersonalVideo'
-import GroupsIcon from '@mui/icons-material/Groups';
+import GroupsIcon from '@mui/icons-material/Groups'
 
 function Sidebar() {
+  
   const location = useLocation();
 
 const menuITems = [
@@ -31,8 +32,9 @@ const menuITems = [
     ]
   },
 ]
+
   return (
-    <aside className="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-[#FEFFFA] dark:bg-slate-600 border-0 shadow-xl dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0 ps z-10">
+    <aside className="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-[#fcfcfc] dark:bg-slate-600 border-0 shadow-xl dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0 ps z-10">
       <nav className="">
         <div className="p-6 shadow-lg  flex justify-between items-center">
           <img src={Logo} alt="" className="" />

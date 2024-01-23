52249b661200c66359521f7d04c249d6b23e4f6f
Victor Manuel Morillo Howley <victormh@IBSYSTEMS.COM>
2024-01-23 12:03:33 -0400

    Fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,9 +14,9 @@ function Navbar() {
     }
   }
   return (
-    <header className="flex p-6  w-screen ">
+    <header className="flex p-6   ">
       <nav className="flex justify-between">
-        <div className="transition-all  w-screen  sm:translate-x-0 flex-col  xl:translate-x-72 z-50 text-white font-extralight">
+        <div className="transition-all w-full sm:translate-x-0 flex-col  xl:translate-x-72 z-50 text-white font-extralight">
           <div className="flex gap-1 ">
             <p className="text-zinc-200">Pages  </p>
             <h1>  /{Location}</h1>
--- src/routes/SubAgent.jsx
+++ src/routes/SubAgent.jsx
@@ -7,7 +7,7 @@ function SubAgent() {
 
   const sendResponse = (e) => {
     e.preventDefault();
-    if (!isNaN(numtar.value) || numtar.length < 1 || numtar.value > 10){ 
+    if (!isNaN(numtar.value)  ){ 
       setResponse({ ...response, message: '1245564', code: '002' });
       document.getElementById('ret-form').reset()
       
@@ -25,7 +25,6 @@ function SubAgent() {
   };
 
   return (
-<<<<<<< HEAD
     <div className="grid p-2 ">
       <form
       id="ret-form"
@@ -209,95 +208,11 @@ function SubAgent() {
               type="text"
               />
           </div>
-=======
-    <div className='flex p-2 '>
-      <form className="w-full shadow-md bg-white dark:bg-slate-800  p-6 rounded-xl z-50">
-  <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
-        Numero de Tarjeta
-      </label>
-      <input onChange={(e) => {setInput({...input, numtc_: e.target.value})}} className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-black border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="xxxx xxxx xxxx xxxx"/>
-    </div>
-    <div className="w-full md:w-1/2 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-last-name">
-        Fecha de Vencimiento
-      </label>
-      <input onChange={(e) => {handleChange(e)}} className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="02/10"/>
-    </div>
-    <div className="w-full md:w-1/2 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-state">
-        Afiliado
-      </label>
-      <div className="relative">
-        <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 dark:text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" disabled>
-          <option>Bomba Next Av.Abraham Lincoln #601</option>
-          <option>Farmacia Carol Av.Abraham Lincoln</option>
-          <option>Super Mercado La Cadena Av.Núñez de Cáceres #400</option>
-        </select>
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 dark:text-black">
-          <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
->>>>>>> 631c40ed9e069c88d293dd69643c7d73f9c55d27
         </div>
       </div>
               </div>
     </div>
-<<<<<<< HEAD
   );
-=======
-    <div className="w-full md:w-1/2 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-last-name">
-        Categoría
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Estacion de combustible"/>
-    </div>
-    <div className="w-full md:w-1/2 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-last-name">
-        Cantide de cuotas
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
-    </div>
-    <div className="w-full md:w-1/2 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-last-name">
-        Moneda
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
-    </div>
-    <div className="w-full md:w-1/2 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-last-name">
-        Tipo de transacción
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
-    </div>
-    <div className="w-full md:w-1/2 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-last-name">
-        Cvv2
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
-    </div>
-  </div>
-  <div className="flex flex-wrap -mx-3 mb-2">
-    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-city">
-        Codigo de respuesta
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque"/>
-    </div>
-    
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2" htmlFor="grid-zip">
-        Numero de autorización
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder={input.numtc_}/>
-    </div>
-  </div>
-  <div className='flex  justify-center mt-6'>
-  <button className='bg-slate-300 bg-opacity-50 rounded-md h-10 w-20 dark:text-white' type='submit' value='submit' >Grabar</button>
-  </div>
-</form>
-    </div>
-  )
->>>>>>> 631c40ed9e069c88d293dd69643c7d73f9c55d27
 }
 
 export default SubAgent;

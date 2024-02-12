673d6ce7714cb83d4eb5b074fa402f497cf85982
vmhowley <vmhowleyh@gmail.com>
2024-02-12 16:33:17 -0400

    Last

--- src/routes/PagoTc.jsx
+++ src/routes/PagoTc.jsx
@@ -38,6 +38,7 @@ function PagoTc () {
           <div className="mt-10 sm:grid sm:grid-cols-2 gap-x-6 gap-y-8">
             <div className="sm:col-span-1">
               <div className="mt-2 ">
+              <label htmlFor="" className="font-bold">Numero de tarjeta</label>
                 <input
                   onChange={(e) => setData({ ...data, tc: e.target.value })}
                   maxLength="16"
@@ -52,6 +53,8 @@ function PagoTc () {
             </div>
             <div className="sm:col-span-1">
               <div className="mt-2">
+              <label htmlFor="" className="font-bold">Moneda</label>
+
                 <input
                   onChange={(e) => setData({ ...data, moneda: e.target.value })}
                   type="text"
@@ -65,6 +68,7 @@ function PagoTc () {
             </div>
             <div className="sm:col-span-1">
               <div className="mt-2">
+                <label htmlFor="" className="font-bold">Monto</label>
                 <input
                   onChange={(e) => setData({ ...data, monto: e.target.value })}
                   id="monto"
@@ -77,7 +81,8 @@ function PagoTc () {
               </div>
             </div>
             <div className={`sm:col-span-1 ${tab === 'reverso' ? 'hidden' : 'show'}`}>
-              <div className="mt-2 grid gap-3">
+              <div className="mt-2 grid ">
+                <label htmlFor="" className="font-bold">Forma de pago</label>
                 <select onChange={(e) => setData({ ...data, foma_pago: e.target.value })} className="block w-full rounded-md border-0 py-2 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ps-2">
                   <option>Forma de pago</option>
                   <option value={1}>Efectivo</option>
@@ -100,6 +105,8 @@ function PagoTc () {
             </div>
             <div className={`col-span-1  ${tab === 'reverso' ? 'show' : 'hidden'}`}>
               <div className="mt-2">
+              <label htmlFor="" className="font-bold">Numero de autorizacion</label>
+
                 <input
                   onChange={(e) => setData({ ...data, autorizacion: e.target.value })}
                   type="text"

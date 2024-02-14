191becf987a1d978e7e311cacb427144223eda65
vmhowley <vmhowleyh@gmail.com>
2024-02-14 16:59:08 -0400

    form

--- src/routes/PagoTc.jsx
+++ src/routes/PagoTc.jsx
@@ -19,22 +19,7 @@ function PagoTc () {
     if (!inputValues.numtar) {
       errors.numtar = 'Email is too short'
     }
-    console.log(errors)
     return errors
-  }
-
-  function ccFormat (value) {
-    const v = value
-      .replace(/\s+/g, '')
-      .replace(/[^0-9]/gi, '')
-      .substr(0, 16)
-    const parts = []
-
-    for (let i = 0; i < v.length; i += 4) {
-      parts.push(v.substr(i, 4))
-    }
-
-    return parts.length > 1 ? parts.join(' ') : value
   }
 
   const handleChange = (e) => {
@@ -45,18 +30,16 @@ function PagoTc () {
     e.preventDefault()
     setErrors(validateValues(data))
     setSubmit(true)
-    finishSubmit()
   }
   const finishSubmit = () => {
     console.log(data)
-    document.getElementById('formi').reset()
-    setData(' ')
+    setData({})
   }
 
   useEffect(() => {
     if (Object.keys(errors).length === 0 && submit) {
-      console.log('nose')
       finishSubmit()
+      setData({})
     }
   }, [errors])
 
@@ -69,37 +52,35 @@ function PagoTc () {
           className="w-full shadow-md dark:bg-[#333b44] p-4 rounded-md bg-white"
         >
           <div className="flex gap-5 h-11 font-semibold">
-            <div type="button" onClick={(e) => setTab('pago')} className={`rounded-lg w-20 cursor-pointer flex justify-center items-center ${tab === 'pago' ? 'bg-blue-100/50 border text-blue-500/90' : ''} `}>
+            <div type="button" onClick={(e) => setTab('pago')} className={`rounded-lg w-20 cursor-pointer flex justify-center items-center ${tab === 'pago' ? 'bg-red-100/50  text-red-500/90' : ''} `}>
               Pago
             </div>
-            <div onClick={(e) => setTab('reverso')} className={`rounded-lg w-20 cursor-pointer flex justify-center items-center ${tab === 'reverso' ? ' bg-blue-100/50 border text-blue-500/90' : ''} `} >
+            <div onClick={(e) => setTab('reverso')} className={`rounded-lg w-20 cursor-pointer flex justify-center items-center ${tab === 'reverso' ? ' bg-red-100/50  text-red-500/90' : ''} `} >
               Reverso
             </div>
 
           </div>
-          <div className="mt-10 sm:grid sm:grid-cols-2 gap-x-6 gap-y-8">
+          <div className="mt-10 sm:grid sm:grid-cols-2 gap-x-6 gap-y-8 text-sm text-slate-600">
             <div className="sm:col-span-1">
               <div className="mt-2 ">
-              <label htmlFor="" className="font-bold">Numero de tarjeta</label>
+              <label htmlFor="" className="font-semibold">Numero de tarjeta</label>
                 <input
                   value={ccFormat(data.numtar) ?? ''}
                   onChange={handleChange}
                   type="tel"
-                  pattern="\d*"
                   maxLength="19"
                   name="numtar"
                   id="numtar"
                   placeholder="xxxx xxxx xxxx xxxx"
                   autoComplete="numtar"
-                  className="block w-full rounded-md border-0 py-1.5 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ps-3"
+                  className="block w-full rounded-md  py-1.5 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 ps-3"
                 />
               </div>
             </div>
             <div className="sm:col-span-1">
               <div className="mt-2">
-              <label htmlFor="" className="font-bold">Moneda</label>
-                <select name="moneda" id="moneda" value={data.moneda ?? ''} onChange={handleChange} className="block w-full rounded-md border-0 py-2 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ps-2">
-                  <option>Seleccionar una moneda</option>
+              <label htmlFor="" className="font-semibold">Moneda</label>
+                <select name="moneda" id="moneda" value={data.moneda ?? ''} onChange={handleChange} className="block w-full rounded-md border-0 py-2 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 ps-2">
                   <option value={214}>DOP</option>
                   <option value={840}>USD</option>
                 </select>
@@ -107,7 +88,7 @@ function PagoTc () {
             </div>
             <div className="sm:col-span-1">
               <div className="mt-2">
-                <label htmlFor="" className="font-bold">Monto</label>
+                <label htmlFor="" className="font-semibold">Monto</label>
                 <input
                   value={data.monto ?? ''}
                   onChange={handleChange}
@@ -115,16 +96,14 @@ function PagoTc () {
                   name="monto"
                   type="monto"
                   autoComplete="monto"
-                  placeholder="Digite el monto"
-                  className="block w-full rounded-md border-0 py-1.5 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ps-3"
+                  className="block w-full rounded-md border-0 py-1.5 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 ps-3"
                 />
               </div>
             </div>
             <div className={`sm:col-span-1 ${tab === 'reverso' ? 'hidden' : 'show'}`}>
               <div className="mt-2 grid ">
-                <label htmlFor="" className="font-bold">Forma de pago</label>
-                <select name="forma_pago" id="forma_pago" value={data.forma_pago ?? ''} onChange={handleChange} className="block w-full rounded-md border-0 py-2 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ps-2">
-                  <option>Seleccionar forma de pago</option>
+                <label htmlFor="" className="font-semibold">Forma de pago</label>
+                <select name="forma_pago" id="forma_pago" value={data.forma_pago ?? ''} onChange={handleChange} className="block w-full rounded-md border-0 py-2 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 ps-2">
                   <option value={1}>Efectivo</option>
                   <option value={2}>Cheque</option>
                 </select>
@@ -132,7 +111,7 @@ function PagoTc () {
             </div>
             <div className={`col-span-1 ${data.forma_pago === '2' && tab === 'pago' ? 'show' : 'hidden'}`}>
               <div className="mt-2">
-                <label htmlFor="cheque" className='font-bold'>Numero de cheque</label>
+                <label htmlFor="cheque" className='font-semibold'>Numero de cheque</label>
                 <input
                   value={data.cheque ?? ''}
                   onChange={handleChange}
@@ -140,14 +119,13 @@ function PagoTc () {
                   name="cheque"
                   id="cheque"
                   autoComplete="numero-cheque"
-                  placeholder="Numero de Cheque"
-                  className="block w-full rounded-md border-0 py-1.5 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ps-3"
+                  className="block w-full rounded-md border-0 py-1.5 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 ps-3"
                 />
               </div>
             </div>
             <div className={`col-span-1  ${tab === 'reverso' ? 'show' : 'hidden'}`}>
               <div className="mt-2">
-              <label htmlFor="" className="font-bold">Numero de autorizacion</label>
+              <label htmlFor="" className="font-semibold">Numero de autorizacion</label>
 
                 <input
                   value={data.autorizacion ?? ''}
@@ -156,20 +134,34 @@ function PagoTc () {
                   name="autorizacion"
                   id="autorizacion"
                   autoComplete="autorizacion"
-                  placeholder="Numero de autorizacion"
-                  className="block w-full rounded-md border-0 py-1.5 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ps-3"
+                  className="block w-full rounded-md border-0 py-1.5 text-slate-600 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 ps-3"
                 />
               </div>
             </div>
           </div>
           <div className="w-full flex justify-center pt-6">
           </div>{' '}
-          <button type="submit" value="submit" className="bg-gray-300 w-16 h-10 font-semibold rounded-full border border-blue-500/30">Send </button>
+          <div className='w-full flex justify-center'>
+          <button type="submit" value="submit" className="bg-red-300/20  h-10 font-semibold rounded-xl p-4 flex items-center  text-red-500">Agregar</button>
+          </div>
         </form>
       </div>
     </>
   )
 }
 
-
 export default PagoTc
+
+function ccFormat (value) {
+  const v = value
+    .replace(/\s+/g, '')
+    .replace(/[^0-9]/gi, '')
+    .substr(0, 16)
+  const parts = []
+
+  for (let i = 0; i < v.length; i += 4) {
+    parts.push(v.substr(i, 4))
+  }
+
+  return parts.length > 1 ? parts.join(' ') : value
+}
--- src/rpgle-sources/GETCUST.rpgle
+++ src/rpgle-sources/GETCUST.rpgle
@@ -1,47 +1,58 @@
 **free
+// Compilar con el siguiente comando:
+// CRTBNDRPG PGM(xyz) SRCFILE(QRPGLESRC)
+// PGMINFO(*PCML)
+// INFOSTMF('/path/to/myfile.pcml')
 
 Ctl-Opt DFTACTGRP(*NO) ACTGRP('WEBAPI') PGMINFO(*PCML:*MODULE);
 
 Dcl-F CUSTFILE Usage(*Input) Keyed PREFIX('CUST.');
+
 Dcl-DS CUST ext extname('CUSTFILE') qualified End-DS;
+
 Dcl-PI *N;
-CustNo like(Cust.Custno);
-Name like(Cust.Name);
-Street like(Cust.Street);
-City like(Cust.City);
-State like(Cust.State);
-Postal like(Cust.Postal);
+    CustNo like(Cust.Custno);
+    Name   like(Cust.Name);
+    Street like(Cust.Street);
+    City   like(Cust.City);
+    State  like(Cust.State);
+    Postal like(Cust.Postal);
 End-PI;
+
 Dcl-PR QMHSNDPM ExtPgm('QMHSNDPM');
-MessageID Char(7) Const;
-QualMsgF Char(20) Const;
-MsgData Char(32767) Const options(*varsize);
-MsgDtaLen Int(10) Const;
-MsgType Char(10) Const;
-CallStkEnt Char(10) Const;
-CallStkCnt Int(10) Const;
-MessageKey Char(4);
-ErrorCode Char(8192) options(*varsize);
+    MessageID Char(7) Const;
+    QualMsgF Char(20) Const;
+    MsgData Char(32767) Const options(*varsize);
+    MsgDtaLen Int(10) Const;
+    MsgType Char(10) Const;
+    CallStkEnt Char(10) Const;
+    CallStkCnt Int(10) Const;
+    MessageKey Char(4);
+    ErrorCode Char(8192) options(*varsize);
+End-PR
 
 Dcl-DS err qualified;
 bytesProv Int(10) inz(0);
 bytesAvail Int(10) inz(0);
 End-DS;
+
 Dcl-S MsgDta Varchar(1000);
 Dcl-S MsgKey Char(4);
 Dcl-S x Int(10);
-chain CustNo CUSTFILE;
+
+ chain CustNo CUSTFILE;
 if not %found;
-msgdta = 'Customer not found.';
-QMHSNDPM( 'CPF9897': 'QCPFMSG *LIBL': msgdta:
+    msgdta = 'Customer not found.';
+    QMHSNDPM( 'CPF9897': 'QCPFMSG *LIBL': msgdta:
 %len(msgdta): '*ESCAPE'
-: '*PGMBDY': 1: MsgKey: err );
+            : '*PGMBDY': 1: MsgKey: err );
 else;
-Custno = Cust.Custno;
-Name = Cust.name;
-Street = Cust.Street;
-City = Cust.City;
-State = Cust.State;
-Postal = Cust.Postal;
+    Custno = Cust.Custno;
+    Name = Cust.name;
+    Street = Cust.Street;
+    City = Cust.City;
+    State = Cust.State;
+    Postal = Cust.Postal;
 endif;
+
 *inlr = *on;
